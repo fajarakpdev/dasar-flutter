267968ff85885b3f43ae2896bcc1198805733091
fajar <fajar@MacBook-Pro-fajar.local>
2024-03-11 00:13:30 +0700

    belajar container widget

--- lib/dasar/home01.dart
+++ lib/dasar/home01.dart
@@ -7,17 +7,21 @@ class Home01 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('App Bar'),
+        title: const Text('Container'),
       ),
-      body: const Center(
-        child: Text('body'),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'profile'),
-        ],
-      ),
+      body: Center(
+          child: Container(
+        color: Colors.yellow,
+        height: 100,
+        width: 200,
+        child: const Text('Container Widget'),
+        alignment: Alignment.center,
+        transform: Matrix4.rotationZ(0.2),
+      )),
+      bottomNavigationBar: BottomNavigationBar(items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home'),
+        BottomNavigationBarItem(icon: Icon(Icons.person), label: 'profile'),
+      ]),
     );
   }
 }
